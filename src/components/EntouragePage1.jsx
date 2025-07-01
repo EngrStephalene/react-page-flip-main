f5a35115a50481288285fc534b513401444797f4
alexflex15 <tanalex257@gmail.com>
2025-07-02 00:17:58 +0800

    Latest change  for entourage
    Latest change  for entourage

--- src/components/EntouragePage1.jsx
+++ src/components/EntouragePage1.jsx
@@ -42,43 +42,6 @@ const EntouragePage1 = () => {
           </ul>
         </div>
       </div>
-
-      <div className="section">
-        <h2 className="subtitle">Best Man</h2>
-        <p className="single-name">Alexander Roque Tan</p>
-        
-        <h2 className="subtitle">Maid of Honor</h2>
-        <p className="single-name">Loucil Ursula Aranas</p>
-      </div>
-
-      <div className="section">
-        <h2 className="subtitle">Groomsmen</h2>
-        <ul className="single-column">
-          <li>Ace Del Rosario</li>
-          <li>Vince Gonzales</li>
-          <li>Joe Talisayan</li>
-          <li>Maolín Carl Valencia</li>
-        </ul>
-      </div>
-
-      <div className="section">
-        <h2 className="subtitle">Bridesmaids</h2>
-        <ul className="single-column">
-          <li>Ren Nicolai Nacional</li>
-          <li>Regilah Vilisette Fabro</li>
-          <li>Klarisse Khrill Calumba</li>
-          <li>Geanne Sophia Zozobrando</li>
-        </ul>
-      </div>
-
-      <div className="section">
-        <h2 className="subtitle">Junior Bridesmaids</h2>
-        <ul className="single-column">
-          <li>Isabela Filomena Aranas</li>
-          <li>Arwa Hussien</li>
-          <li>Leona Jhunnahia Aranas</li>
-        </ul>
-      </div>
     </div>
   );
 };
--- src/components/EntouragePage4.jsx
+++ src/components/EntouragePage4.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from 'react';
 
 const EntouragePage4 = () => {
   return (
-    <div>EntouragePage4</div>
-  )
-}
+    <div className="entourage-page">
+      <div className="section">
+        <h2 className="subtitle">Best Man</h2>
+        <p className="single-name">Alexander Roque Tan</p>
 
-export default EntouragePage4
+        <h2 className="subtitle">Maid of Honor</h2>
+        <p className="single-name">Loucil Ursula Aranas</p>
+      </div>
+
+      <div className="section">
+        <h2 className="subtitle">Groomsmen</h2>
+        <ul className="single-column">
+          <li>Ace Del Rosario</li>
+          <li>Vince Gonzales</li>
+          <li>Joe Talisayan</li>
+          <li>Maolín Carl Valencia</li>
+        </ul>
+      </div>
+
+      <div className="section">
+        <h2 className="subtitle">Bridesmaids</h2>
+        <ul className="single-column">
+          <li>Ren Nicolai Nacional</li>
+          <li>Regilah Vilisette Fabro</li>
+          <li>Klarisse Khrill Calumba</li>
+          <li>Geanne Sophia Zozobrando</li>
+        </ul>
+      </div>
+
+      <div className="section">
+        <h2 className="subtitle">Junior Bridesmaids</h2>
+        <ul className="single-column">
+          <li>Isabela Filomena Aranas</li>
+          <li>Arwa Hussien</li>
+          <li>Leona Jhunnahia Aranas</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default EntouragePage4;
