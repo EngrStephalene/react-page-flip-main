47582a404dab7e9e4d5876e9aa99ed85cf40f7af
Stephalene Tan <57058331+EngrStephalene@users.noreply.github.com>
2025-07-02 20:45:57 +0800

    fix background

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -13,60 +13,63 @@ import LocationPage from './LocationPage';
 import EntouragePage1 from './EntouragePage1';
 import EntouragePage2 from './EntouragePage2';
 import EntouragePage4 from './EntouragePage4';
+import background from '../../images/background.jpg'
 
 // Page component with forwardRef correctly set up
 const Page = forwardRef(({ number, children }, ref) => (
   <div className="demoPage" ref={ref}>
     {/* <h1>R&I</h1> */}
+    <div className='Alex'>
     <p>{children}</p>
+    </div>
     {/* <p>Page number: {number}</p> */}
   </div>
 ));
 
 function Book() {
   return (
-    <HTMLFlipBook
-      width={800}
-      height={1000}
-      maxShadowOpacity={0.5}
-      drawShadow={true}
-      showCover={true}
-      size="fixed"
-    >
-      <Page number={1}>
-        <CoverPage/>
-      </Page>
-      <Page number={2}>
-        <FirstImage/>
-      </Page>
-      <Page number={3}>
-        <FirstImageDesc/>
-      </Page>
-      <Page number={4}>
-        <OurStory/>
-      </Page>
-      <Page number={5}>
-        <OurStoryVid/>
-      </Page>
-      <Page number={6}>
-        <ThemePage/>
-      </Page>
-      <Page number={7}>
-        <LocationPage/>
-      </Page>
-      <Page number={8}>
-        <EntouragePage1/>
-      </Page>
-      <Page number={9}>
-        <EntouragePage2/>
-      </Page>
-      <Page number={10}>
-        <EntouragePage4/>
-      </Page>
-      <Page number={11}>
-        
-      </Page>
-    </HTMLFlipBook>
+      <HTMLFlipBook
+        width={800}
+        height={1000}
+        maxShadowOpacity={0.5}
+        drawShadow={true}
+        showCover={true}
+        size="fixed"
+      >
+        <Page number={1}>
+          <CoverPage/>
+        </Page>
+        <Page number={2}>
+          <FirstImage/>
+        </Page>
+        <Page number={3}>
+          <FirstImageDesc/>
+        </Page>
+        <Page number={4}>
+          <OurStory/>
+        </Page>
+        <Page number={5}>
+          <OurStoryVid/>
+        </Page>
+        <Page number={6}>
+          <ThemePage/>
+        </Page>
+        <Page number={7}>
+          <LocationPage/>
+        </Page>
+        <Page number={8}>
+          <EntouragePage1/>
+        </Page>
+        <Page number={9}>
+          <EntouragePage2/>
+        </Page>
+        <Page number={10}>
+          <EntouragePage4/>
+        </Page>
+        <Page number={11}>
+          
+        </Page>
+      </HTMLFlipBook>
   );
 }
 
